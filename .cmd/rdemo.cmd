a434cbe677f65e5f09687f468f9de78422ac5fa4
Terry M. Poulin <BigBoss1964@gmail.com>
2014-03-03 16:47:16 +0000

    Remove sxe specific demo commands.

--- .cmd/rdemo.cmd
+++
@@ -1,1 +0,0 @@
-@CALL "%SXE_ROOT%\.cmd\rd.cmd" %*
--- envsetup.sh
+++ envsetup.sh
@@ -187,40 +187,6 @@ mma() { # Builds all of the modules in the current directory, and their dependen
 # }
 
 
-rd() { # :run a demo by name
-    m ":demos:${1}:pc:run"
-    # TODO: pass args to app as a property?
-}
-
-
-rdemo() { # alias for rd
-    rd $*
-}
-
-
-idemo() { # :installApp a demo by name
-    m ":demos:${1}:pc:installApp"
-}
-
-
-irdemo() { # installdemo and execute a demo by name with following args.
-    local demo
-    demo=$1
-    shift
-    idemo $demo
-    eval \
-        env \
-            XDG_DATA_DIRS="`gettop`/demos/$demo/pc/src/dist/share" \
-            XDG_CONFIG_DIRS="`gettop`/demos/$demo/pc/src/dist/etc" \
-            XDG_DATA_HOME="`gettop`/tmp/share" \
-            XDG_CONFIG_HOME="`gettop`/tmp/config" \
-            XDG_CACHE_HOME="`gettop`/tmp/cache" \
-            "`echo $demo | awk '{print toupper($0)}'`_PC_OPTS=\"'-Djava.library.path=$(gettop)/demos/${demo}/pc/build/install/${demo}-pc/lib/natives'\"" \
-            \
-                "$(gettop)/demos/${demo}/pc/build/install/${demo}-pc/bin/${demo}-pc" "$@"
-}
-
-
 jgrep() { # runs grep on all local Java source files.
     find . -name .git -prune -o -type f -name "*\.java" -print0 | xargs -0 grep --color -n "$@"
 }
@@ -319,4 +285,3 @@ PROJECT_NAME="$(basename $(pwd))"
 EOF
 fi
 
-lsproj
