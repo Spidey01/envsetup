798a7cdeb1edf7902e908e66b4339577129f944c
Terry M. Poulin <BigBoss1964@gmail.com>
2014-08-17 14:07:28 -0400

    Call it v1.4.4.

--- envsetup.sh
+++ envsetup.sh
@@ -24,7 +24,7 @@
 #
 # For more information, please refer to <http://unlicense.org>
 
-ENVSETUP_VERSION="1.4.3"
+ENVSETUP_VERSION="1.4.4"
 
 # database like list of project files, see 'filelist'.
 # path is relative to projet root.
